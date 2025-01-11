4d0d1ed376fadb14579eac3abb75cb5fd45f59d3
Walla coding <122129717+w-coding@users.noreply.github.com>
2025-01-11 20:03:22 +0300

    new update

---
+++ app/src/main/java/com/dya/noor/utlis/QuranPageUtils.java
@@ -0,0 +1,13 @@
+package com.dya.noor.utlis;
+
+public class QuranPageUtils {
+
+    public static String ActivityName ;
+    public static String folderName ;
+    public static String SuraName ;
+    public static int SuraId ;
+    public static int pageNumber ;
+    public static boolean fileExist = false; ;
+
+
+}
